23833ba647412c3cad2a528e73e64741eb4c4b22
rock1200pq-pixel <rock1200pq@gmail.com>
2026-03-05 11:59:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
 function passwordGen(){
-
 let pass=Math.random().toString(36).slice(-10);
-
-alert("Generated Password: "+pass);
-
+alert("Generated Password: " + pass);
 }
 
 function textCounter(){
-
 let text=prompt("Enter text");
-
-alert("Characters: "+text.length);
-
+alert("Characters: " + text.length);
 }
 
 function qrGenerator(){
-
 let text=prompt("Enter text for QR");
 
 window.open(
 "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="+text
 );
-
 }
 
 function mergePDF(){
-
 alert("PDF Merge Tool Coming Soon");
-
 }
 
 function imageToPDF(){
-
 alert("Image to PDF Tool Coming Soon");
-
 }
