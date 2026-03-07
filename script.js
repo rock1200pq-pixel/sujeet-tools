e009135e9cc48715e17820b0612f0fd2908b0280
rock1200pq-pixel <rock1200pq@gmail.com>
2026-03-07 10:57:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,24 +37,3 @@ document.getElementById("qr").innerHTML=
 "<img src='https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="+text+"'>";
 
 }
-function rollDice(){
-
-let dice=document.getElementById("dice");
-let result=document.getElementById("dice-result");
-
-let number=Math.floor(Math.random()*6)+1;
-
-let rotations={
-1:"rotateX(0deg) rotateY(0deg)",
-2:"rotateX(-90deg)",
-3:"rotateY(90deg)",
-4:"rotateY(-90deg)",
-5:"rotateX(90deg)",
-6:"rotateY(180deg)"
-};
-
-dice.style.transform=rotations[number];
-
-result.innerText="You rolled: "+number;
-
-}
